4cb6182efb0ad660b7015e6ca7bb491226daa6f7
Aisha Kimpson <akimp313@gmail.com>
2022-05-14 22:58:20 -0400

    added handlebars

--- server.js
+++ server.js
@@ -14,3 +14,27 @@ app.use(
     },
   })
 );
+
+app.engine(
+  "hbs",
+  hbs({
+    extname: "hbs",
+    defaultLayout: "layout",
+    layoutsDir: _dirname + "/views/layouts",
+  })
+);
+app.set("view engine", "hbs");
+
+app.use((req, res, next) => {
+  if (req.cookies.user_sid && !req.session.user) {
+    res.clearCookies("user_sid");
+  }
+  next();
+});
+
+var hbsContent = {
+  userName: "",
+  loggedin: false,
+  title: "You are not logged-in users",
+  body: "Welcome",
+};
