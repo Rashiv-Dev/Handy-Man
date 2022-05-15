c425bff2b2be0fa868d8c73b60200b08c63c979d
Aisha Kimpson <akimp313@gmail.com>
2022-05-15 02:07:20 -0400

    added server.js comments

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
+// express application
 var app = express();
+
+// set application port
 app.set("port", 9000);
+
+// set morgan to log info about our requests for development use.
 app.use(morgan("dev"));
+
+// initialize body-parser to parse incoming parameters requests to req.body
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// initialize cookie-parser to allow us access the cookies stored in the browser.
 app.use(cookieParser());
+
+// initialize express-session to allow us track the logged-in user across sessions.
 app.use(
   session({
     key: "user_sid",
@@ -15,6 +26,7 @@ app.use(
   })
 );
 
+// handle bars config
 app.engine(
   "hbs",
   hbs({
@@ -23,8 +35,10 @@ app.engine(
     layoutsDir: _dirname + "/views/layouts",
   })
 );
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
-
+// This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
+// This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
 app.use((req, res, next) => {
   if (req.cookies.user_sid && !req.session.user) {
     res.clearCookies("user_sid");
@@ -128,6 +142,7 @@ app.use(function (req, res, next) {
   res.status(404).send("Not Found");
 });
 
+// start the express server
 app.listen(app.get("port"), () =>
   console.log(`App started on port ${app.get("port")}`)
 );
