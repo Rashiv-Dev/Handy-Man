44d361bdd42b7468fb239da4aa2b1e6df4f05d40
Aisha Kimpson <akimp313@gmail.com>
2022-05-18 21:27:32 -0400

    removed conflicting code to merge

--- db/comment_seeds.sql
+++
@@ -1,8 +0,0 @@
-USE skillbuild_db;
-
-INSERT INTO comment (company_name, about_comment)
-VALUES 
-('Grassy Knoll Landscaping", 'Great job!'), 
-(`Inside & Out Painters`, '5 Star Job!',) 
-(`Raise The Roofing Company`, 'I would definitely recommend.'), 
-(`Pipes & Leaks Plumbing Service`, 'Awesome guys! Thank you for your help!'), 
