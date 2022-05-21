2f45adb13fb374bafaa1be40ee06ce9d1e69dbe1
robertarthury <robertarthury@gmail.com>
2022-05-20 20:53:31 -0400

    clean up dirty file

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -8,258 +8,208 @@ const commentdata = [
   },
   {
     comment_text: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
-    customeremail: ''
+    customeremail: 'satcat@gmail.com'
   },
   {
     comment_text: 'Aliquam erat volutpat. In congue.',
-    user_id: 3,
-    post_id: 10
+    customeremail: 'maskcat@gmail.com'
   },
   {
     comment_text: 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
-    user_id: 3,
-    post_id: 18
+    customeremail: 'packckat@gmail.com'
   },
   {
     comment_text: 'In hac habitasse platea dictumst.',
-    user_id: 7,
-    post_id: 5
+    customeremail: 'pink_lemon34@gmail.com'
   },
   {
     comment_text: 'Vivamus vestibulum sagittis sapien.',
-    user_id: 1,
-    post_id: 20
+    customeremail: 'sashgt67@monster.com'
   },
   {
     comment_text: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
-    user_id: 6,
-    post_id: 7
+    customeremail: 'sashabcohen@gmail.com'
   },
   {
     comment_text: 'Sed vel enim sit amet nunc viverra dapibus.',
-    user_id: 7,
-    post_id: 4
+    customeremail: 'kiarra_harvey67@gmail.com'
   },
   {
     comment_text: 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat.',
-    user_id: 6,
-    post_id: 12
+    customeremail: 'laverne27@hotmail.com'
   },
   {
     comment_text: 'Morbi a ipsum.',
-    user_id: 6,
-    post_id: 20
+    customeremail: 'linda_collins74@yahoo.com'
   },
   {
     comment_text: 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
-    user_id: 3,
-    post_id: 14
+    customeremail: 'claudine_harvey74@yahoo.com'
   },
   {
     comment_text: 'Donec ut mauris eget massa tempor convallis.',
-    user_id: 5,
-    post_id: 4
+    customeremail: 'albin_daniel@hotmail.com'
   },
   {
-    comment_text:
-      'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.',
-    user_id: 4,
-    post_id: 9
+    comment_text: 'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.',
+    customeremail: 'kasandra.kshlerin@gmail.com'
   },
   {
     comment_text:
       'Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.',
-    user_id: 5,
-    post_id: 14
+    customeremail: 'flavie_breitenberg@hotmail.com'
   },
   {
     comment_text: 'Quisque porta volutpat erat.',
-    user_id: 6,
-    post_id: 2
+    customeremail: 'gwen49@yahoo.com'
   },
   {
     comment_text: 'Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.',
-    user_id: 8,
-    post_id: 2
+    customeremail: ''
   },
   {
-    comment_text:
-      'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est.',
-    user_id: 2,
-    post_id: 20
+    comment_text: 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est.',
+    customeremail: 'magali_schuster79@yahoo.com'
   },
   {
     comment_text: 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis.',
-    user_id: 4,
-    post_id: 11
+    customeremail: 'dallin.dooley@gmail.com'
   },
   {
     comment_text:
       'Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est.',
-    user_id: 5,
-    post_id: 13
+    customeremail: 'crawford_mitchell@hotmail.com'
   },
   {
     comment_text:
       'Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.',
-    user_id: 9,
-    post_id: 16
+    customeremail: 'domenico.zulauf@hotmail.com'
   },
   {
     comment_text: 'Curabitur convallis.',
-    user_id: 6,
-    post_id: 4
+    customeremail: 'triston31@gmail.com'
   },
   {
     comment_text: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
-    user_id: 4,
-    post_id: 10
+    customeremail: 'caden_dickinson25@gmail.com'
   },
   {
     comment_text: 'Morbi non quam nec dui luctus rutrum.',
-    user_id: 3,
-    post_id: 8
+    customeremail: 'hannah96@gmail.com'
   },
   {
     comment_text:
       'Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique.',
-    user_id: 8,
-    post_id: 10
+    customeremail: 'amir.simonis@gmail.com'
   },
   {
     comment_text:
       'Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.',
-    user_id: 1,
-    post_id: 15
+    customeremail: 'luz70@gmail.com60'
   },
   {
     comment_text: 'Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend.',
-    user_id: 5,
-    post_id: 3
+    customeremail: 'america33@yahoo.com'
   },
   {
     comment_text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
-    user_id: 1,
-    post_id: 15
+    customeremail: 'zander.olson@hotmail.com'
   },
   {
     comment_text: 'Nam tristique tortor eu pede.',
-    user_id: 4,
-    post_id: 16
+    customeremail: 'charlene_willms@gmail.com'
   },
   {
     comment_text: 'Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.',
-    user_id: 4,
-    post_id: 18
+   customeremail: 'francesca_hettinger@yahoo.com'
   },
   {
     comment_text: 'Proin eu mi. Nulla ac enim.',
-    user_id: 4,
-    post_id: 10
+    customElements: 'darrin18@gmail.com'
   },
   {
     comment_text: 'Sed ante. Vivamus tortor.',
-    user_id: 7,
-    post_id: 5
+    customeremail: 'aileen32@yahoo.com'
   },
   {
     comment_text: 'Aliquam quis turpis eget elit sodales scelerisque.',
-    user_id: 10,
-    post_id: 1
+    customeremail: 'ettie17@gmail.com'
   },
   {
     comment_text: 'Donec quis orci eget orci vehicula condimentum.',
-    user_id: 3,
-    post_id: 19
+    customeremail: 'luz.howell62@gmail.com'
   },
   {
     comment_text: 'Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.',
-    user_id: 5,
-    post_id: 3
+    customeremail: 'ocie_hackett84@gmail.com'
   },
   {
     comment_text: 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst.',
-    user_id: 10,
-    post_id: 14
+    customeremail: 'lloyd_hyatt@hotmail.com'
   },
   {
     comment_text: 'Maecenas ut massa quis augue luctus tincidunt.',
-    user_id: 10,
-    post_id: 8
+    customeremail: 'norris.oconner14@gmail.com'
   },
   {
     comment_text: 'Cras in purus eu magna vulputate luctus.',
-    user_id: 10,
-    post_id: 11
+    customeremail: 'johnpaul49@hotmail.com'
   },
   {
     comment_text: 'Etiam vel augue. Vestibulum rutrum rutrum neque.',
-    user_id: 8,
-    post_id: 5
+    customeremail: 'berniece0@yahoo.com'
   },
   {
     comment_text: 'Proin at turpis a pede posuere nonummy.',
-    user_id: 8,
-    post_id: 19
+    customeremail: 'davion.pagac@gmail.com'
   },
   {
     comment_text: 'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl.',
-    user_id: 9,
-    post_id: 19
+    customeremail: 'isaac_hessel7@hotmail.com'
   },
   {
     comment_text:
       'Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.',
-    user_id: 5,
-    post_id: 4
+    customeremail: 'kaitlin17@yahoo.com'
   },
   {
     comment_text:
       'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc.',
-    user_id: 2,
-    post_id: 11
+    customeremail: 'ricky47@hotmail.com'
   },
   {
     comment_text: 'Vestibulum ac est lacinia nisi venenatis tristique.',
-    user_id: 4,
-    post_id: 6
+   customeremail: 'ned.sauer71@hotmail.com'
   },
   {
     comment_text: 'Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.',
-    user_id: 9,
-    post_id: 6
+    customeremail: 'madie92@hotmail.com'
   },
   {
     comment_text:
       'Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam.',
-    user_id: 7,
-    post_id: 9
+    customeremail: 'jarrell_schroeder@gmail.com'
   },
   {
     comment_text: 'Integer ac leo. Pellentesque ultrices mattis odio.',
-    user_id: 4,
-    post_id: 19
+    customeremail: 'rogers97@yahoo.com'
   },
   {
     comment_text: 'Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.',
-    user_id: 10,
-    post_id: 1
+    customeremail: 'pablo_gorczany86@gmail.com'
   },
   {
     comment_text:
       'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus.',
-    user_id: 2,
-    post_id: 19
+   customeremail: 'nellie_ondricka@hotmail.com'
   },
   {
     comment_text: 'Proin risus. Praesent lectus.',
-    user_id: 10,
-    post_id: 1
+    customeremail: 'pierre68@hotmail.com'
   },
   {
     comment_text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus.',
-    user_id: 10,
-    post_id: 12
+   customeremail: 'sigrid_simonis94@yahoo.com'
   }
 ];
 
