d204056b6e161ee1da0907be9126132fd47721c4
Rashiv-Dev <81837453+Rashiv-Dev@users.noreply.github.com>
2022-05-22 19:19:38 -0500

    home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -8,10 +8,30 @@ router.get('/signup', (req,res)=>{
     res.render('signup')
 })
 router.get('/dashboard', withAuth, (req, res) =>{
-    res.render('dashboard')
+    try{
+        const dbUserData = await User.findAll({
+            include:[ {
+                model:Comment, Ads
+            },
+        ],
+        });
+        const users = dbUserData.map((user)=> user.get({plain:true}));
+   
+    res.render('dashboard',{
+        users,
+        loggedIn: req.session.loggedIn,
+    })
+    }
+    catch (err) {
+        crossOriginIsolated.log(err);
+        res.status(500).json(err);
+    }
 });
+
 router.get('/ads',withAuth, (req, res) => {
-    res.render('ads');
+    res.render('ads',{
+        loggedIn: req.session.loggedIn,
+    });
 });
 
 module.exports = router;
