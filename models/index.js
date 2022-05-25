fb07dc8dad4dc91637333edaa04bd67ad3d85ae8
chris chubb <chubb91chris@icloud.com>
2022-05-24 22:10:46 -0400

    added associations need to fix handle bars. login works

--- models/index.js
+++ models/index.js
@@ -2,7 +2,8 @@ const Ads = require('./Ads');
 const User = require('./User');
 const Comments = require('./Comments');
 
-
-
-
+User.hasMany(Comments);
+User.hasMany(Ads);
+Ads.belongsTo(User);
+Comments.belongsTo(User)
 module.exports = {Ads,User,Comments};
